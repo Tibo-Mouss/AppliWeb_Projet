7daa9c89a8c21cf33b7a15fdb75e86633bf483dd
Alexandre Koch <alexandrekoch3@gmail.com>
2022-05-13 11:38:18 +0200

    Update Review.java

--- src/main/java/org/jboss/as/Review.java
+++ src/main/java/org/jboss/as/Review.java
@@ -1,5 +1,43 @@
 package org.jboss.as;
 
+@Entity
 public class Review {
     
+    @Id, @GeneratedValue
+    private int id_review;
+    private String comment;
+    private int mark;
+
+    @ManyToOne
+    private User author_com;
+
+    public Review(){}
+
+    public int getId(){
+        return this.id_review;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
+    public int getMark(){
+        return this.mark;
+    }
+
+    public User getAuthor(){
+        return this.author_com;
+    }
+
+    public void setComment(String com){
+        this.comment = com;
+    }
+
+    public void setMark(int mark){
+        this.mark = mark;
+    }
+
+    public void setAuthor(User mec){
+        this.author_com = mec;
+    }
 }
