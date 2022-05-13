48d0b52e8bbf855ebca9dbf5bf57a7ee908a4b81
Edgar Remy <83783043+edgaremy@users.noreply.github.com>
2022-05-13 11:37:48 +0200

    User

--- src/main/java/org/jboss/as/User.java
+++ src/main/java/org/jboss/as/User.java
@@ -1,5 +1,43 @@
 package org.jboss.as;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     
+    private String username;
+    private String password;
+    private List<Manga> favorites = new ArrayList<>();
+    
+    public User(){}
+
+    public User(String usr, String pswd){
+        this.username = usr;
+        this.password = pswd;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public List<Manga> getFavorites(){
+        return favorites;
+    }
+
+    public void setUsername(String usr){
+        this.username = usr;
+    }
+
+    public void setPassword(String pswd){
+        this.password = pswd;
+    }
+
+
+
+    
+
 }
